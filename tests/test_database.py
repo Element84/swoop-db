6d878a915d1f259c3d3abced233eb68ad52d25f5
Neeraj Sirdeshmukh <sirneeraj@gmail.com>
2023-06-14 16:54:32 -0500

    testing if fixture can be applied to database

--- tests/test_database.py
+++ tests/test_database.py
@@ -14,3 +14,13 @@ async def test_has_pgtap_db(pgtap_db) -> None:
 @pytest.mark.asyncio
 async def test_verify(pg_dump) -> None:
     assert await SwoopDB().verify(pg_dump=pg_dump)
+
+@pytest.mark.asyncio
+async def test_load_fixture() -> None:
+    try:
+        await SwoopDB().load_fixture('base_01')
+    except Exception as exc:
+        assert False, f"Fixture could not be applied to database: {exc}"
+
+
+
