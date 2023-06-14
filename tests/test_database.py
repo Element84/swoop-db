3668fbb4c25666521585a7e4bb3f88f02a185025
Neeraj Sirdeshmukh <sirneeraj@gmail.com>
2023-06-14 17:17:44 -0500

    fix database creation

--- tests/test_database.py
+++ tests/test_database.py
@@ -18,7 +18,11 @@ async def test_verify(pg_dump) -> None:
 @pytest.mark.asyncio
 async def test_load_fixture() -> None:
     try:
-        await SwoopDB().load_fixture('base_01')
+        test_db = SwoopDB()
+        await test_db.create_database('swoop')
+        await test_db.load_schema()
+        await test_db.load_fixture('base_01')
+        await test_db.drop_database('swoop')
     except Exception as exc:
         assert False, f"Fixture could not be applied to database: {exc}"
 
