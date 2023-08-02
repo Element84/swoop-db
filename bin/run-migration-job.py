4a931a1e39352ebce595bf87ff078b3ace9ee718
Neeraj Sirdeshmukh <sirneeraj@gmail.com>
2023-08-02 15:59:55 -0500

    updates to migration job

--- bin/run-migration-job.py
+++ bin/run-migration-job.py
@@ -14,19 +14,18 @@ It requires the following environment variables be set:
     - PGPORT:
           port number of the database container
     - PGDATABASE: name of the database which will be used for migrations
+    - ACTION: name of the action performed, either migrate or rollback
+    - VERSION: the migration version to which to migrate/rollback the database
+    - NO_WAIT: override option to skip waiting for active connections to close.
     - Any additional libpq-supported connection parameters
           (https://www.postgresql.org/docs/current/libpq-envars.html)
 """
-import argparse
 import asyncio
 import os
 import sys
-
-from buildpg import V, funcs, render
+import time
 
 from swoop.db import SwoopDB
-
-APPLICATION_ROLES = ["user_api", "user_caboose", "user_conductor"]
 
 
 def stderr(*args, **kwargs) -> None:
@@ -34,77 +33,70 @@ def stderr(*args, **kwargs) -> None:
     print(*args, **kwargs)
 
 
-def check_positive(value) -> int:
-    ivalue = int(value)
-    if ivalue <= 0:
-        raise argparse.ArgumentTypeError(
-            "%s is an invalid migration version number. Only \
-                positive values are supported."
-            % value
-        )
-    return ivalue
+def strtobool(val) -> bool:
+    """Convert a string representation of truth to true or false.
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    'val' is anything else.
+    """
+    val = val.lower()
+    if val in ("y", "yes", "t", "true", "on", "1"):
+        return True
+    elif val in ("n", "no", "f", "false", "off", "0"):
+        return False
+    else:
+        raise ValueError("invalid truth value %r" % (val,))
 
 
 async def run_migrations() -> None:
     dbname: str = os.environ["PGDATABASE"]
 
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument(
-        "--action",
-        choices=["migrate", "rollback"],
-        help="An action to take, either migrate or rollback",
-    )
-    parser.add_argument(
-        "--version",
-        type=check_positive,
-        help="The migration version to which to migrate/rollback the database",
-    )
-
-    parser.add_argument(
-        "--wait_override",
-        choices=["true", "false"],
-        help="Override option to skip waiting for active connections to close. \
-        If specified, it is true, and if not specified it is false.",
-    )
-
-    args = parser.parse_args()
-
-    action = args.action
-    version = args.version
-    override = args.wait_override
+    action = os.environ["ACTION"]
+    version = int(os.environ["VERSION"])
+    no_wait = strtobool(os.environ["NO_WAIT"])
 
     swoop_db = SwoopDB()
 
-    if override == "true":
-        stderr(f"Applying migrate/rollback on database {dbname} to version {version}")
-        await swoop_db.migrate(target=version, database=dbname)
-    else:
-        async with swoop_db.get_db_connection(database="") as conn:
-            # Wait for all active connections from user roles to be closed
-            active = True
-            roles_list = V("usename") == funcs.any(APPLICATION_ROLES)
-            while active:
-                q, p = render(
-                    "SELECT * FROM pg_stat_activity WHERE datname = :db AND :un_clause",
-                    db=dbname,
-                    un_clause=roles_list,
+    async with swoop_db.get_db_connection() as conn:
+        # Wait for all active connections from user roles to be closed
+        active_sessions = not no_wait
+        while active_sessions:
+            active_sessions = await conn.fetchval(
+                """
+                SELECT EXISTS(
+                    SELECT * FROM pg_stat_activity
+                    WHERE
+                        datname = current_database()
+                        AND usename != current_user
                 )
+                """,
+            )
+            if active_sessions:
+                time.sleep(2)
 
-                records = await conn.fetch(q, *p)
-                if len(records) == 0:
-                    active = False
+        current_version = await swoop_db.get_current_version(conn=conn)
+        version_compatible = True
 
-            if action == "migrate":
-                stderr(f"Migrating database {dbname} to version {version}")
-            elif action == "rollback":
-                stderr(f"Rolling back database {dbname} to version {version}")
-            else:
-                stderr(
-                    f"Applying migrate/rollback on database {dbname} to \
-                    version {version}"
-                )
-            await swoop_db.migrate(target=version, database=dbname)
+        if action == "rollback":
+            stderr(f"Rolling back database {dbname} to version {version}")
+            direction = "down"
+            if current_version is not None:
+                version_compatible = version <= current_version
+        else:
+            stderr(f"Migrating database {dbname} to version {version}")
+            direction = "up"
+            if current_version is not None:
+                version_compatible = version >= current_version
+
+        if version_compatible:
+            await swoop_db.migrate(
+                target=version, direction=direction, database=dbname, conn=conn
+            )
+        else:
+            stderr(
+                f"The current version of the database is incompatible with the desired"
+                "target version {version} and action {action}"
+            )
 
 
 if __name__ == "__main__":
